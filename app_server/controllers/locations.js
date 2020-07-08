df059056b27dfc2da1e32d14c52883a612b71089
Ann Stepp <annlstepp@gmail.com>
2020-07-08 11:31:26 -0500

    Adding the view templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,81 @@
 /* GET 'home' page */
 const homelist = (req,res) => {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find places to work with wifi near you!'
+      },
+      locations: [{ 
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      }, {
+        name: 'Cat Cafe',
+        address: '345 Garfield Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Cats!'],
+        distance: '200m'   
+      }, {
+        name: 'Ann Place',
+        address: '789 Essex Drive, Gladstone, 64156',
+        rating: 5,
+        facilities: ['Cocktails', 'Snacks', 'Premium wifi'],
+        distance: '400m'  
+      }]
+    });
 };
 /* GET 'Location info' page */
 const locationInfo = (req, res) => {
-    res.render('location-info', { title: 'Location info'});
+  res.render('location-info', {
+    title: 'Starcups',
+    pageHeader: {title: 'Starcups'},   
+    sidebar: {
+        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+        callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot Drinks', 'Food', 'Premium wifi'],
+      coords: {lat: 51.455041, lng: -0.9690884},
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      },{
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false  
+      },{
+        days: 'Sunday',
+        closed: true      
+      }],
+      reviews: [{
+        author: 'Simon Homes',
+        rating: 5,
+        timestamp: '16 july 2013',
+        reviewText: 'What a great place. I can\'t say enoung good things about it'    
+      },{
+        author: 'Charlie Chaplin',
+        rating: 3,
+        timestamp: '16 July 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'   
+      }]
+    }
+  });        
 };
 /* GET 'Add review' page */
 const addReview = (req, res) => {
-    res.render('location-review-form', {title: 'Add review'});
+    res.render('location-review-form', {
+      title: 'Review Starcups on Loc8r',
+      pageHeader: { title: 'Review Starcups' }
+    });
 };
 module.exports = {
     homelist,
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,9 @@
 /* GET 'about' page. */
 const about = (req, res) => {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+      title: 'About Loc8r',
+      content: 'Loc8r was created to help people find places to sid down and get a bit of work done. <br/><br/>Lorem ipsum dolor sit amet, est et vidit mundi decore, wisi liberavisse necessitatibus te ius, quo dicam aeterno et. Ius vivendo maluisset signiferumque ad, eam et sonet invidunt. Te unum sale autem qui, pro nisl repudiare constituto cu. Deserunt voluptatum duo id, sea te euismod ponderum intellegam, nulla tincidunt eos ex. Ea est utamur tritani signiferumque, eum cu tale nulla, qui ei mutat falli voluptatibu.'
+    });
   };
 
   module.exports = {
